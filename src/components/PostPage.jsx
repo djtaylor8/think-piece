d05512c82a5160b9da09eca646b1f5f3acf672cb
DJ <dtaylor88@gmail.com>
2022-01-08 13:12:51 -0800

    add hoc for user to add comments

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -4,19 +4,66 @@ import Comments from './Comments';
 
 import { firestore } from '../firebase';
 import { collectIdsAndDocs, withRouter } from '../utilities';
+import withUser from './withUser';
 
 
 class PostPage extends Component {
     state = { post: null, comments: [] };
 
+    get postId() {
+        return this.props.router.params.id;
+    }
+
+    get postRef() {
+        return firestore.doc(`posts/${this.postId}`);
+    }
+
+    get commentsRef() {
+        return this.postRef.collection('comments');
+    }
+
+    unsubscribeFromPost = null;
+    unsubscribeFromComments = null;
+
+    componentDidMount = async () => {
+        this.unsubscribeFromPost = this.commentsRef.onSnapshot(snapshot => {
+            const comments = snapshot.docs.map(collectIdsAndDocs);
+            this.setState({ comments });
+        })
+
+        this.unsubscribeFromComments = this.postRef.onSnapshot(snapshot => {
+            const post = collectIdsAndDocs(snapshot);
+            this.setState({ post });
+        })
+    }
+
+    componentWillUnmount = () => {
+        this.unsubscribeFromPost();
+        this.unsubscribeFromComments();
+    }
+
+    createComment = (comment) => {
+        const { user } = this.props
+        this.commentsRef.add({
+            ...comment,
+            user,
+        })
+    }
+    
+    
+
     render() {
-        console.log(this.props);
+        const { post, comments } = this.state;
         return (
             <div>
-                Post page!
+                {post && <Post {...post} />}
+                <Comments 
+                  comments={comments}
+                  onCreate={this.createComment}
+                />
             </div>
         );
     }
 }
 
-export default withRouter(PostPage);
+export default withRouter(withUser((PostPage)));
