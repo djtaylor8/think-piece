807b76e798d6b3658a36a92c4e28c6b803045e0f
DJ <dtaylor88@gmail.com>
2022-01-08 11:58:10 -0800

    refactor userprofile to clsass component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { auth, firestore } from '../firebase';
 
-const UserProfile = () => {
+class UserProfile extends Component {
+    state = { displayName: '' };
+    imageInput = null;
+
+    get uid() {
+        return auth.currentUser.uid;
+    }
+
+    get userRef() {
+        return firestore.doc(`users/${this.uid}`);
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { displayName } = this.state;
+
+        if (displayName) {
+            this.userRef.update({ displayName });
+        }
+    }
+    render() {
+
     return (
         <div>
-            This is totall a user profile page!
+            
         </div>
     );
+  };
 };
 
 export default UserProfile;
